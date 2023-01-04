025e92d9189591970d866f62c1a3eccc8dfd13e0
Danelle <rhowia@gmail.com>
2023-01-03 19:38:44 -0500

    Got one controlled component to work but just displays to console

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,18 +3,34 @@ import React, { Component } from 'react';
 class Main extends Component {
     constructor(props) {
         super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        console.log(this.state.value);
+        event.preventDefault();
     }
 
     render() {
         return (
-            <p>Hello, {this.props.name}.</p>     
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" 
+                    value={this.state.value}
+                    onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>     
         );
     }
 }
 
 export default Main
-
-
-{/*<p>{this.props.title}</p>
-<p>{this.props.email}</p>
-<p>{this.props.phoneNumber}</p>     */}
