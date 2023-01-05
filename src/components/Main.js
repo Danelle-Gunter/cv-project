a23b74572f68c83de051842afaf4551ebb241d1d
Danelle <rhowia@gmail.com>
2023-01-05 11:17:02 -0500

    Working at submitting multiple fields

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,29 +4,54 @@ import React, { Component } from 'react';
 class Main extends Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {
+            value: '',
+            //lastName: '',
+        };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({value: event.target.value});
+        //const target = event.target;
+        //const name = target.name;
+        
+        this.setState({
+            value: event.target.value,
+            //lastName: event.target.lastName
+        });
     }
 
-    handleSubmit() {
+    handleSubmit(event) {
         document.getElementById('result').textContent = this.state.value;
+        event.preventDefault();
     }
 
     render() {
         return (
             <div>
-                <label>Name:</label>
-                <input type="text" 
-                        value={this.state.value}
-                        onChange={this.handleChange} />
-                <button onClick={this.handleSubmit}>Submit Info</button>
-                
+                <form>
+                    <label>
+                        First Name:
+                        <input
+                            name='firstName' 
+                            type="text" 
+                            value={this.state.value}
+                            onChange={this.handleChange} />
+                    </label>
+                    <br />
+                    {/* <label>
+                        Last Name:
+                        <input
+                            name='lastName' 
+                            type='text'
+                            value={this.state.lastName}
+                            onChange={this.handleChange} />
+                    </label>
+                    <br />     */}
+                    <button onClick={this.handleSubmit}>Submit Info</button>
+                </form>
                 <hr />
                 <p id="result"></p>    
             </div>     
