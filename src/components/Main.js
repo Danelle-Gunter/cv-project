b92f7030ed76bb7cda2522b4f4bba831ea87dfa1
Danelle <rhowia@gmail.com>
2023-01-05 10:55:07 -0500

    Got a submitted element to appear on the page after submit

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,24 +14,21 @@ class Main extends Component {
         this.setState({value: event.target.value});
     }
 
-    handleSubmit(event) {
-        event.preventDefault();
+    handleSubmit() {
+        document.getElementById('result').textContent = this.state.value;
     }
 
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Name:
-                        <input type="text" 
+                <label>Name:</label>
+                <input type="text" 
                         value={this.state.value}
                         onChange={this.handleChange} />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
-                <hr />
+                <button onClick={this.handleSubmit}>Submit Info</button>
                 
+                <hr />
+                <p id="result"></p>    
             </div>     
         );
     }
